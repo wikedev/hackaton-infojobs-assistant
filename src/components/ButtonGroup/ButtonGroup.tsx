8cc4c4552196af04c61e7f4246bd7c2354e7d92e
Wike Developer <wikedev@gmail.com>
2023-05-30 14:12:07 +0200

    Add style to ButtonGroup

--- src/components/ButtonGroup/ButtonGroup.tsx
+++ src/components/ButtonGroup/ButtonGroup.tsx
@@ -6,8 +6,8 @@ export default function ButtonGroup({ options, selection, handleSelection }: { o
       {options.map((option, index) => {
 
         const style = option === selection ?
-          "h-12 w-1/3  border-primary border bg-primary text-white" :
-          "h-12 w-1/3  border-primary border bg-white text-primary"
+          "h-12 w-1/3  border-primary border bg-primary text-white font-medium" :
+          "h-12 w-1/3  border-primary border bg-white text-primary font-medium"
 
         return (
           <button key={index} onClick={() => handleSelection(option)} className={style}>
