d785cf5ff86c7e3cd9a0972115ff374eb706e97b
Wike Developer <wikedev@gmail.com>
2023-05-30 13:41:52 +0200

    Add function to Input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -5,7 +5,7 @@ export default function Input({ request, handleRequest, handleGPT }: { request: 
 
       <div className="relative">
 
-        <input type="search" id="search" className=" block w-full p-4 pzl-10  border text-black border-primary rounded-md " placeholder="Compara mi CV con las ofertas actuales" onChange={e => handleRequest(e.target.value)} value={request} />
+        <input type="search" id="search" onClick={()=>{handleRequest("")}} className=" block w-full p-4 pzl-10  border text-black border-primary rounded-md " placeholder="Compara mi CV con las ofertas actuales" onChange={e => handleRequest(e.target.value)} value={request} />
         <button onClick={() => handleGPT()} className="text-white absolute right-2.5 bottom-2.5 bg-accent hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ENVIAR</button>
       </div>
 
